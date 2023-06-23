d736a30ffe7ea05f2dd8ed088093f1a32bcbd98f
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-23 06:28:27 -0500

    i don´t now why I deleted that lines... lost code restoration

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //importacion de módulo global http
 const http = require('http');
-
+const f = require('fs');
 function requestlistener(req, res){
 //puede ser una function anonima
 }
+
+//requestlistener 
+
 //creacion de servidor web
 /* http.createServer(requestlistener); */
 /* http.createServer(function (req, res){
@@ -13,10 +16,20 @@ function requestlistener(req, res){
 const server = http.createServer((req, res)=>{
     const url = req.url;
     /* console.log(req.url, req.method, req.headers); */
+    //cerrar o salir del ciclo continuo de eventos
     /* procces.exit(); */
 
 //routing
 
+if(url ==="/"){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head> <title>My first response page</title> </head>');
+    res.write('<Body><form action="/message" method="Post"> <input type="text" name="message"><button type="submit" >Send</button></form> </Body>');
+    res.write('<html>');
+    res.write('</html>');
+    return res.end();
+}
     //enviando respuestas
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
